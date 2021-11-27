3fc13b513a7b69b6bc7ae87def5f432fd56dfb4c
SebastianDerendarz <Derendarz99@gmail.com>
2021-11-27 13:18:17 +0100

    1.3.2 Fix formatting and convention errors 1-3

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,21 @@
 package edu.kis.vh.nursery;
 
+//https://cezarywalenciuk.pl/blog/programing/skorty-kalwiszowe-eclipse
+//Alt + ← and Alt + → Wracasz do miejsc, które wcześniej odwiedzałeś.
+
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
+
 }
