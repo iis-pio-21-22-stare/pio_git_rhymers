8d1e88a8d43c3184de1ef9d7137bf5b2da6c4176
kmynek <dawidek150499456Dk>
2021-12-10 11:25:32 +0100

    comment at Rhymersfactory.java

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -2,14 +2,17 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
 
-	public interface Rhymersfactory {
-	
-		public defaultCountingOutRhymer GetStandardRhymer();
-		
-		public defaultCountingOutRhymer GetFalseRhymer();
-		
-		public defaultCountingOutRhymer GetFIFORhymer();
-		
-		public defaultCountingOutRhymer GetHanoiRhymer();
-		
-	}
+public interface Rhymersfactory {
+
+    defaultCountingOutRhymer GetStandardRhymer();
+
+    defaultCountingOutRhymer GetFalseRhymer();
+
+    defaultCountingOutRhymer GetFIFORhymer();
+
+    defaultCountingOutRhymer GetHanoiRhymer();
+
+}
+
+// połączenie klawiszy alt + (strzałka) służy szybkim przełączaniu się
+//pomiędzy otwartmi kartami programów w środowisku
