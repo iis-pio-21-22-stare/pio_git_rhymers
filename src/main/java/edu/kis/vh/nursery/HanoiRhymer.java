e8ab6f6b8a9ae77763e4acaade3614ed719724a9
Magickinder <kampaw24@gmail.com>
2022-01-09 19:55:00 +0100

    1.3.5 (4) - corrected class names
    Rebased class-method-naming with master.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultCountingOutRhymer {
 	// Wiersze 6, 13, 15, 16 były źle sformatowane
 
 	int totalRejected = 0;
