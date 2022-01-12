80f63e3eae634806aa587f8725fc81223eaf0e7e
szczotszczot <szczotj@gmail.com>
2022-01-12 15:00:49 +0100

    1.3.5 fixed classes names

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,21 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer defaultCountingOutRhymer = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
         while (!callCheck())
 
-            defaultCountingOutRhymer.countIn(super.countOut());
+            temp.countIn(super.countOut());
 
-        int result = defaultCountingOutRhymer.countOut();
+        int ret = temp.countOut();
 
-        while (!defaultCountingOutRhymer.callCheck())
+        while (!temp.callCheck())
 
-            countIn(defaultCountingOutRhymer.countOut());
+            countIn(temp.countOut());
 
-        return result;
+        return ret;
     }
 }
