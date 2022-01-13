e538728c3ea8547bcae14de5bf294ae4dd6b8d15
kmynek <dawkoc@gmail.com>
2022-01-14 00:17:25 +0100

    stale_adnotacje_final

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 // alt + strzałka -> sprawne przemieszczanie się pomiędzy plikami w  projekcie
-public class FIFORhymer extends DefaultCountingOutRhymer {
+ final public class FIFORhymer extends DefaultCountingOutRhymer {
 
     public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends DefaultCountingOutRhymer {
+final public class HanoiRhymer extends DefaultCountingOutRhymer {
     static final int ZERO=0;
     int totalRejected = ZERO;
 
