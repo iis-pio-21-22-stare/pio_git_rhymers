cc2558dc14d4e6c8e92c04c0596981f66751febb
Rafał <rafixmix@interia.pl>
2022-01-14 11:14:53 +0100

    1.4 stale 3 - dodanie annotacji @Override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,25 +2,26 @@ package edu.kis.vh.nursery;
 // alt + -> służy łatwemu przemieszczaniu się pomiędzy plikami w projekcie
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-public static final int REJECTED_VALUE = 0;
-int totalRejected = REJECTED_VALUE;
+	public static final int REJECTED_VALUE = 0;
+	int totalRejected = REJECTED_VALUE;
 
-public int reportrejected() {
-	return getTotalRejected();
-}
-
-public void countin(int in) {
-if (!callcheck() && in > peekaboo())
-		setTotalRejected(getTotalRejected() + 1);
-		else
-			super.countin(in);
-}
-
-	public int getTotalRejected() {
-		return totalRejected;
+	public int reportrejected() {
+		return getTotalRejected();
 	}
 
-	public void setTotalRejected(int totalRejected) {
-		this.totalRejected = totalRejected;
+	@Override
+	public void countin(int in) {
+	if (!callcheck() && in > peekaboo())
+			setTotalRejected(getTotalRejected() + 1);
+			else
+				super.countin(in);
 	}
+
+		public int getTotalRejected() {
+			return totalRejected;
+		}
+
+		public void setTotalRejected(int totalRejected) {
+			this.totalRejected = totalRejected;
+		}
 }
