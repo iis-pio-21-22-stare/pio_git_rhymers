c5d987f21c6e427b57966775d93873d81298d594
Szymon228880 <228880@edu.p.lodz.pl>
2022-01-14 11:42:01 +0100

    1.4 stale 1 zmiana liczb na stale

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,10 +1,11 @@
 package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
-    public static final int size_tab = 12;
+    public static final int Size_Tab = 12;
     public static final int ErrCheck = -1;
-    public static final int numb = 11;
-    private  final int[] numbers = new int[size_tab];
+    public static final int Numb = 11;
+    // mam nadzieje ze wypiles kawe ktora ci zapazylem wczorajszego dnia, jak tam brzuszek ?
+    private  final int[] numbers = new int[Size_Tab];
 
     public int total = -1;
 
@@ -18,7 +19,7 @@ public class defaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == numb;
+        return total == Numb;
     }
 
     protected int peekaboo() {
