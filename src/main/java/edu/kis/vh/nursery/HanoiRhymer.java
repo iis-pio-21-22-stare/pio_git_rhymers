532f33b96b045b07ab452f2cdf040c8ee66d471e
SzuligowskiM <szuligowskim@gmail.com>
2022-01-14 11:09:37 +0100

    1.4 stale 1 zastopiono literaly deklaracjami stalych

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int REJECTED_COUNT = 0;
+    int totalRejected = REJECTED_COUNT;
 
     public int reportRejected() {
         return totalRejected;
