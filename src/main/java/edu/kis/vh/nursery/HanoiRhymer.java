abf69f30c214c996e1bab0c827f67c0550dd7593
Rafał <rafixmix@interia.pl>
2022-01-14 11:10:53 +0100

    1.4 stale 1 - zadeklarowanie stałych

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 // alt + -> służy łatwemu przemieszczaniu się pomiędzy plikami w projekcie
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-private int totalRejected = 0;
+public static final int REJECTED_VALUE = 0;
+int totalRejected = REJECTED_VALUE;
 
 public int reportrejected() {
 	return getTotalRejected();
