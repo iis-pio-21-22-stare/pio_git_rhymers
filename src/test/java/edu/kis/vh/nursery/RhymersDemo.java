6b4629d58be58f7558ef3e2eebc822e93ff2d758
kmynek <dawkoc@gmail.com>
2022-01-14 02:16:52 +0100

    Utworzenie metody statycznej testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,26 +12,30 @@ class RhymersDemo {
 	private static void main(String[] args) {
 	Rhymersfactory factory = new DefaultRhymersFactory();
 
-	DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-			factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+		testRhymers(factory);
 
-	for (int i = 1; i < FIVETEEN; i++)
-		for (int j = 0; j < THREE; j++)
-			rhymers[j].countin(i);
-
-	java.util.Random rn = new java.util.Random();
-	for (int i = 1; i < FIVETEEN; i++)
-		rhymers[3].countin(rn.nextInt(TWENTY));
-
-	for (int i = 0; i < rhymers.length; i++) {
-		while (!rhymers[i].callcheck())
-			System.out.print(rhymers[i].countout() + "  ");
-		System.out.println();
 	}
 
-	System.out.println("total rejected is "
-			+ ((HanoiRhymer) rhymers[3]).reportrejected());
+	private static void testRhymers(Rhymersfactory factory) {
+		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 
-}
+		for (int i = 1; i < FIVETEEN; i++)
+			for (int j = 0; j < THREE; j++)
+				rhymers[j].countin(i);
+
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < FIVETEEN; i++)
+			rhymers[3].countin(rn.nextInt(TWENTY));
+
+		for (int i = 0; i < rhymers.length; i++) {
+			while (!rhymers[i].callcheck())
+				System.out.print(rhymers[i].countout() + "  ");
+			System.out.println();
+		}
+
+		System.out.println("total rejected is "
+				+ ((HanoiRhymer) rhymers[3]).reportrejected());
+	}
 
 }
