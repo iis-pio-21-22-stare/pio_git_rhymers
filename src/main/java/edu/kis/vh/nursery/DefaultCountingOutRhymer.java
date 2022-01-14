1eef8241a067d522df9b6e82d2bf48b86c02394b
KamilSzus <kamil-szustak1@wp.pl>
2022-01-14 11:18:03 +0100

    1.4 stale 2
    Zamiana modyfikator final przy niemutowalnych zmiennych i parametrach klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,11 +6,11 @@ public class DefaultCountingOutRhymer {
 	public static final int STARTING_VALUE = -1;
 	public static final int LAST_ELEMENT = 11;
 
-	private int[] numbers = new int[SIZE];
+	private final int[] numbers = new int[SIZE];
 
 	public int total = STARTING_VALUE;
 
-	public void countIn(int in) {
+	public void countIn(final int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
