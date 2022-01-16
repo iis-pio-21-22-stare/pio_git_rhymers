2fe5293a7c4cc9e890ce3ccf6ea9cb0b2bf99258
228717 <228717@edu.p.lodz.pl>
2022-01-16 19:44:23 +0100

    1.4 - stale 1

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private final int[] numbers = new int[12];
+    public static final int NUMBERS_ARRAY_SIZE = 12;
+    public static final int TOTAL_START_VALUE = -1;
+    public static final int FULL_VALUE = 11;
+    public static final int ERROR = -1;
+    private int[] numbers = new int[NUMBERS_ARRAY_SIZE];
 
-    public int total = -1;
+    public int total = TOTAL_START_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_START_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_VALUE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total--];
     }
 
