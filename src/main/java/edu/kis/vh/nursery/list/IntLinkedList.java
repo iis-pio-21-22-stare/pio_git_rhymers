b3f185ed928b87509f085a3ebd234049dca17657
Michał Hechłacz <michalhec@gmail.com>
2022-01-17 00:32:55 +0100

    1.4 accesory 3 Hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -3,16 +3,16 @@ package edu.kis.vh.nursery.list;
 public class IntLinkedList {
 
 	private Node last;
-	int i;
+	private int i;
 	private final int empty = -1;
 
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
 		}
 	}
 
@@ -27,15 +27,22 @@ public class IntLinkedList {
 	public int top() {
 		if (isEmpty())
 			return empty;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return empty;
-		int ret = last.value;
-		last = last.prev;
+		int ret = last.getValue();
+		last = last.getPrev();
 		return ret;
 	}
 
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
 }
