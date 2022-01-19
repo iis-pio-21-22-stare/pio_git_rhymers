40364d7308c99d63d4c88b63de1559abea527b46
Kamil <kamildymecki2000@interia.pl>
2022-01-19 09:19:48 +0100

    1.4 accessory 2 - Dodanie gettera dla total w DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,6 +10,10 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[NUMBERS_SIZE];
     //tablica numbers została zmieniona na final podczas poprzednich laboratoriów
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
