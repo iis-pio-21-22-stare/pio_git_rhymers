452b30bf728dbc122359c5c1cd1ee644c4cf1b85
karol <karol@lea24.pl>
2022-01-24 10:23:47 +0100

    1.5.6

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,13 +2,26 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * counting out class
+     */
+
     int totalRejected = 0;
 
     public int reportRejected() {
+        /**
+         * return total rejected int
+         */
         return totalRejected;
     }
 
     public void countIn(int in) {
+
+        /**
+         * counting total rejected
+         * @param in input number to count in
+         */
+
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
