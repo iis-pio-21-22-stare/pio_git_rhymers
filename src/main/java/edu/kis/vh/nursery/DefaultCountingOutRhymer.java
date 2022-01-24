aaa24f664ac6fe41ccb2bd67cb4361eb53b831e3
BarFil <222397@edu.p.lodz.pl>
2022-01-24 19:03:49 +0100

    1.4 accesory 1 zmiana modyfikatorów public, private, etc.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE_OF_ARRAY = 12;
-    public static final int CHECK = -1;
+    private static final int SIZE_OF_ARRAY = 12;
+    private static final int CHECK = -1;
     private final int[] numbers = new int[SIZE_OF_ARRAY];
 
-    public int total = CHECK;
+    private int total = CHECK;
 
     public void countIn(int in) {
         if (!isFull())
