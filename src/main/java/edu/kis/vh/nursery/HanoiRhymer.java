aba7d0e384eabcd4b61f89ba5007db9d08325dc1
kmynek <dawkoc@gmail.com>
2022-01-24 21:43:41 +0100

    1.4 - Accessory -> 1. -> Analiza oraz zmiana widoczności (modyfikatory public,private)

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -11,22 +11,16 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 private int totalRejected = 0;
 
 public int reportrejected() {
-	return getTotalRejected();
+	return totalRejected;
 }
 
 @Override
 public void countin(final int in) {
 if (!callcheck() && in > peekaboo())
-		setTotalRejected(getTotalRejected() + 1);
+		totalRejected++;
 		else
 			super.countin(in);
 }
 
-	public int getTotalRejected() {
-		return totalRejected;
-	}
 
-	public void setTotalRejected(int totalRejected) {
-		this.totalRejected = totalRejected;
-	}
 }
