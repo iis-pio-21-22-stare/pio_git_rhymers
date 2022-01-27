4236d5298fabe8c76e3b6798cdca63ed08531fe6
TomaszOnopa <tomekonopa.pl@gmail.com>
2022-01-27 16:18:03 +0100

    1.4 accesory 1 - zamiana modyfikatorów atrybutów klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int SET_TOTAL = -1;
+    private static final int SIZE = 12;
+    private static final int SET_TOTAL = -1;
     private final int[] numbers = new int[SIZE];
 
-    public int total = SET_TOTAL;
+    private int total = SET_TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
