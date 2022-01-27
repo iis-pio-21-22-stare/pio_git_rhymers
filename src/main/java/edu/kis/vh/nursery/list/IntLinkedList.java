bb01f302e40e231d8c497887b3ce76c144fe74fc
domin <dominika32110@gmail.com>
2022-01-27 20:41:11 +0100

    1.4 accesory 3, hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,22 +2,26 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-	public static final int RETURN_VALUE = -1;
+	private static final int RETURN_VALUE = -1;
 	private Node last;
 	private int i;
 
+	public static int getReturnValue() {
+		return RETURN_VALUE;
+	}
+
 	public void push(final int i) {
-		if (last == null)
-			last = new Node(i);
+		if (getLast() == null)
+			setLast(new Node(i));
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			getLast().setNext(new Node(i));
+			getLast().getNext().setPrev(getLast());
+			setLast(getLast().getNext());
 		}
 	}
 
 	public boolean isEmpty() {
-		return last == null;
+		return getLast() == null;
 	}
 
 	public boolean isFull() {
@@ -26,16 +30,31 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return RETURN_VALUE;
-		return last.value;
+			return getReturnValue();
+		return getLast().getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return RETURN_VALUE;
-		final int ret = last.value;
-		last = last.prev;
+			return getReturnValue();
+		final int ret = getLast().getValue();
+		setLast(getLast().getPrev());
 		return ret;
 	}
 
+	public Node getLast() {
+		return last;
+	}
+
+	public void setLast(Node last) {
+		this.last = last;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
 }
