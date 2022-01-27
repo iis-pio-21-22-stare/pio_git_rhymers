8c58651aa2a1ca657a405fd0d971136cafc86b09
Veanden <marcin.bros17@gmail.com>
2022-01-27 18:56:26 +0100

    1.4 accessory 4 manually removed unused setters, Navigate -> Open Call Hierarchy is not working for me :(

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -46,12 +46,4 @@ public class IntLinkedList {
 	public void setLast(Node last) {
 		this.last = last;
 	}
-
-	public int getI() {
-		return i;
-	}
-
-	public void setI(int i) {
-		this.i = i;
-	}
 }
