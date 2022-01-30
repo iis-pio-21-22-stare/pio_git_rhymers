db01a2abe1240d05573988f8be5b09761a2fc2c3
Bartłomiej Sikora <72757737+LenoBartek@users.noreply.github.com>
2022-01-30 13:21:20 +0100

    1.4.2 Ustawienie modyfikatora final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
 	public static final int ARRAY_SIZE = 12;
 	public static final int ERROR_CODE = -1;
 
-	private int[] numbers = new int[ARRAY_SIZE];
+	private final int[] numbers = new int[ARRAY_SIZE];
 	public int total = ERROR_CODE;
 
 	public void countIn(int in) {
