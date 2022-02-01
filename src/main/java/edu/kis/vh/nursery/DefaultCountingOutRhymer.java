bbb053a9e00a579345e1a6944c2063c1cdf2c093
Przemyslaw Olszewski <229299@edu.p.lodz.pl>
2022-02-01 19:04:28 +0100

    1.4 stale 1 - Zamiana literalow na stale

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int ARRAY_SIZE = 12;
+	public static final int EMPTY_ARRAY_INDEX = -1;
+	public static final int FULL_ARRAY_INDEX = 11;
+	private int[] numbers = new int[ARRAY_SIZE];
 
-	public int total = -1;
+	public int total = EMPTY_ARRAY_INDEX;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == EMPTY_ARRAY_INDEX;
 	}
 		
 	public boolean isFull() {
-		return total == 11;
+		return total == FULL_ARRAY_INDEX;
 	}
 		
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return EMPTY_ARRAY_INDEX;
 		return numbers[total];
 	}
 			
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return EMPTY_ARRAY_INDEX;
 	return numbers[total--];
 	}
 
