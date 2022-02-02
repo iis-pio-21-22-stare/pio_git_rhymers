a4b0cf086748324c5f058c86e8088e0663039aa8
Jakub Mościcki <225344@edu.p.lodz.pl>
2022-02-02 19:43:07 +0100

    1.4 stale 1 - stworzenie stalych by zastapic literaly

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int ARAY_SIZE = 12;
+    public static final int LAST_INDEX_OF_ARAY = 11;
+    public static final int FIRST_INDEX_OF_EMPTY_ARAY = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[ARAY_SIZE];
+
+    public int total = FIRST_INDEX_OF_EMPTY_ARAY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == FIRST_INDEX_OF_EMPTY_ARAY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == LAST_INDEX_OF_ARAY;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return FIRST_INDEX_OF_EMPTY_ARAY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return FIRST_INDEX_OF_EMPTY_ARAY;
         return numbers[total--];
     }
 
