9365426aa07eadbfc26c93575246cbebff535545
CrazyChimken <adam.pietrasik90@gmail.com>
2022-02-03 13:09:18 +0100

    1.5.4 corrected tests and added new one

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -12,19 +12,19 @@ public class RhymersJUnitTest {
         rhymer.countIn(testValue);
 
         int result = rhymer.peekaboo();
-        Assert.assertEquals(testValue, result);
+        Assert.assertEquals("Value should be " + testValue + ", but is " + result, testValue, result);
     }
 
     @Test
     public void testCallCheck() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
         boolean result = rhymer.callCheck();
-        Assert.assertEquals(true, result);
+        Assert.assertTrue("Value should be " + true + ", but is " + result, result);
 
         rhymer.countIn(888);
 
         result = rhymer.callCheck();
-        Assert.assertEquals(false, result);
+        Assert.assertFalse("Value should be " + false + ", but is " + result, result);
     }
 
     @Test
@@ -33,12 +33,12 @@ public class RhymersJUnitTest {
         final int STACK_CAPACITY = 12;
         for (int i = 0; i < STACK_CAPACITY; i++) {
             boolean result = rhymer.isFull();
-            Assert.assertEquals(false, result);
+            Assert.assertFalse("Value should be " + false + ", but is " + result, result);
             rhymer.countIn(888);
         }
 
         boolean result = rhymer.isFull();
-        Assert.assertEquals(true, result);
+        Assert.assertTrue("Value should be " + true + ", but is " + result, result);
     }
 
     @Test
@@ -47,15 +47,15 @@ public class RhymersJUnitTest {
         final int EMPTY_STACK_VALUE = -1;
 
         int result = rhymer.peekaboo();
-        Assert.assertEquals(EMPTY_STACK_VALUE, result);
+        Assert.assertEquals("Value should be " + EMPTY_STACK_VALUE + ", but is " + result, EMPTY_STACK_VALUE, result);
 
         int testValue = 4;
         rhymer.countIn(testValue);
 
         result = rhymer.peekaboo();
-        Assert.assertEquals(testValue, result);
+        Assert.assertEquals("Value should be " + testValue + ", but is " + result, testValue, result);
         result = rhymer.peekaboo();
-        Assert.assertEquals(testValue, result);
+        Assert.assertEquals("Value should be " + testValue + ", but is " + result, testValue, result);
     }
 
     @Test
@@ -64,15 +64,38 @@ public class RhymersJUnitTest {
         final int EMPTY_STACK_VALUE = -1;
 
         int result = rhymer.countOut();
-        Assert.assertEquals(EMPTY_STACK_VALUE, result);
+        Assert.assertEquals("Value should be " + EMPTY_STACK_VALUE + ", but is " + result, EMPTY_STACK_VALUE, result);
 
         int testValue = 4;
         rhymer.countIn(testValue);
 
         result = rhymer.countOut();
-        Assert.assertEquals(testValue, result);
+        Assert.assertEquals("Value should be " + testValue + ", but is " + result, testValue, result);
         result = rhymer.countOut();
-        Assert.assertEquals(EMPTY_STACK_VALUE, result);
+        Assert.assertEquals("Value should be " + EMPTY_STACK_VALUE + ", but is " + result, EMPTY_STACK_VALUE, result);
+    }
+
+    @Test
+    public void testFIFORhymerCountOut(){
+        FIFORhymer rhymer = new FIFORhymer();
+        final int EMPTY_STACK_VALUE = -1;
+
+        int result = rhymer.countOut();
+
+        Assert.assertEquals("Value should be " + EMPTY_STACK_VALUE + ", but is " + result, EMPTY_STACK_VALUE, result);
+    }
+
+    @Test
+    public void testReportRejected(){
+        HanoiRhymer rhymer = new HanoiRhymer();
+        final int STACK_CAPACITY = 12;
+        for(int i = 0; i < STACK_CAPACITY; ++i){
+            rhymer.countIn(i);
+        }
+
+        int result = rhymer.reportRejected();
+
+        Assert.assertEquals("Value should be " + 3 + ", but is " + result, STACK_CAPACITY - 1, result);
     }
 
 }
