a43840845c46d3fcd51d688446fb1c38e4ba3499
szczotszczot <szczotj@gmail.com>
2022-02-04 02:54:04 +0100

    1.5.1 Extract Method testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,27 +11,31 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 	 private static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
-		
-		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-		
-		for (int i = 1; i < SIZE; i++)
-			for (int j = 0; j < POSITION; j++)
-				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < SIZE; i++)
-			rhymers[POSITION].countIn(rn.nextInt(NEXT_INT));
-		
-		for (int i = 0; i < rhymers.length; i++) {
-			while (!rhymers[i].callCheck())
-				System.out.print(rhymers[i].countOut() + "  ");
-			System.out.println();
-		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[POSITION]).reportRejected());
-		
-	}
-	
-}
+
+		 testRhymers(factory);
+
+	 }
+
+	 private static void testRhymers(RhymersFactory factory) {
+		 DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+				 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+
+		 for (int i = 1; i < SIZE; i++)
+			 for (int j = 0; j < POSITION; j++)
+				 rhymers[j].countIn(i);
+
+		 java.util.Random rn = new java.util.Random();
+		 for (int i = 1; i < SIZE; i++)
+			 rhymers[POSITION].countIn(rn.nextInt(NEXT_INT));
+
+		 for (DefaultCountingOutRhymer rhymer : rhymers) {
+			  while (!rhymer.callCheck())
+				  System.out.print(rhymer.countOut() + "  ");
+			  System.out.println();
+		 }
+
+		 System.out.println("total rejected is "
+				 + ((HanoiRhymer) rhymers[POSITION]).reportRejected());
+	 }
+
+ }
